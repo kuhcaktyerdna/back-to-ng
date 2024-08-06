aefccc02b6332b5c79d25fae91c222b73b24f2a7
Andrey Tkachuk <chkaayendutrk@gmail.com>
2024-08-06 20:24:09 +0300

    Created components structure
    MIL-1 Create footer and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { FooterComponent } from "./core/footer/footer.component";
+import { HeaderComponent } from "./core/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
