013a8b65e88340866001e125b86fa662359c2d75
Andrey Tkachuk <chkaayendutrk@gmail.com>
2024-08-09 20:50:55 +0300

    * Created ui input component * added fontawesome
    MIL-1 Create footer and header

--- src/app/ui/components/input/input.component.ts
+++ src/app/ui/components/input/input.component.ts
@@ -3,12 +3,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'ui-input',
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.scss'
 })
 export class InputComponent {
 
   @Input()
   placeholder?: string;
+
+  @Input()
+  withButton?: boolean;
+
+  @Output()
+  valueChange: EventEmitter<string> = new EventEmitter<string>();
 
   @Output()
   submit: EventEmitter<string> = new EventEmitter();
@@ -18,5 +24,6 @@ export class InputComponent {
       const searchStr: string = (<HTMLInputElement>event.target)?.value;
       this.submit.emit(searchStr);
     }
+
   }
 }
