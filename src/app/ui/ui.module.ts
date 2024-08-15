e97c30683363c3dd39007c5580d500e13eb808ec
Andrey Tkachuk <chkaayendutrk@gmail.com>
2024-08-15 22:24:36 +0300

    Created ui component dropdown
    MIL-1 Create footer and header

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { IconComponent } from "./components/icon/icon.component";
 import { InputComponent } from "./components/input/input.component";
 import { ButtonComponent } from "./components/button/button.component";
+import { FormsModule } from "@angular/forms";
+import { DropdownComponent } from "./components/dropdown/dropdown.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 const UI_COMPONENTS = [
   ButtonComponent,
+  DropdownComponent,
   IconComponent,
   InputComponent
 ];
 
 @NgModule({
   declarations: UI_COMPONENTS,
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule
+  ],
   exports: UI_COMPONENTS
 })
 export class UiModule {
