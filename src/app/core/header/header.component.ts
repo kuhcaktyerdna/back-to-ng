cdaca55c1bc9215523af064dce5c2dee89d1840d
Andrey Tkachuk <chkaayendutrk@gmail.com>
2024-09-10 01:24:45 +0300

    Implemented header layout with dropdowns
    MIL-1 Create footer and header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  faCartShopping,
+  faHeadset,
+  faHeart,
+  faSearch,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-header',
@@ -7,7 +16,30 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  protected readonly faHeadset: IconDefinition = faHeadset;
+  protected readonly faSearch: IconDefinition = faSearch;
+  protected readonly faUser: IconDefinition = faUser;
+  protected readonly faHeart: IconDefinition = faHeart;
+  protected readonly faCartShopping: IconDefinition = faCartShopping;
+
+  protected readonly userForm: FormGroup = new FormGroup({
+      userEmail: new FormControl("", [
+        Validators.required,
+        Validators.email
+      ]),
+      userPassword: new FormControl("", [])
+    },
+  );
+
   search(searchStr: string): void {
     console.log(searchStr);
+  }
+
+  signIn(): void {
+    console.log(`signIn ${this.userForm.value}`);
+  }
+
+  signUp(): void {
+    console.log(`signUp ${this.userForm.value}`);
   }
 }
