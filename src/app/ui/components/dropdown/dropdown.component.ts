461f99b5a138a564386213e18a36f2e0b7b66797
Andrey Tkachuk <chkaayendutrk@gmail.com>
2026-05-22 02:32:21 +0300

    feat: fixed dropdown component logic

--- src/app/ui/components/dropdown/dropdown.component.ts
+++ src/app/ui/components/dropdown/dropdown.component.ts
@@ -1,17 +1,38 @@
-import { Component, Input } from '@angular/core';
-import { NgClass } from "@angular/common";
+import { Component, Input, OnDestroy, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'ui-dropdown',
   templateUrl: './dropdown.component.html',
-  imports: [
-    NgClass
-  ],
   styleUrl: './dropdown.component.scss'
 })
 export class DropdownComponent {
 
   @Input()
   openOnHover: boolean = false;
+
+  protected isOpen: WritableSignal<boolean> = signal(false);
+
+  protected onMouseEnter(): void {
+    if (this.openOnHover) {
+      this.isOpen.set(true);
+    }
+  }
+
+  protected onMouseLeave(event: MouseEvent): void {
+    if (!this.openOnHover) {
+      return;
+    }
+    // relatedTarget is null when mouse moves to browser-native UI (e.g. Chrome autofill popup)
+    if (!event.relatedTarget) {
+      return;
+    }
+    this.isOpen.set(false)
+  }
+
+  protected toggle(): void {
+    if (this.openOnHover) {
+      this.isOpen.update(v => !v);
+    }
+  }
 
 }
